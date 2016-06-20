265be4b6e51b64c643c447c8a25e238655d1e64b
Mark C. Kosirog <mark.kosirog@vg-mbp-kosirog.local>
2016-06-20 15:01:02 -0500

    Allow for entries to be added

--- collegeData/ViewController.swift
+++ collegeData/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate
     {
+        @IBAction func onTappedPlusButton(sender: UIBarButtonItem)
+        {
+            let alert = UIAlertController(title: "Add College", message: nil, preferredStyle: .Alert)
+            alert.addTextFieldWithConfigurationHandler { (textField) -> Void in
+                textField.placeholder = "Add College Here"
+            }
+            let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+            alert.addAction(cancelAction)
+            let addAction = UIAlertAction(title: "Add", style: .Default){ (action) -> Void in
+            let collegeTextField = alert.textFields![0] as UITextField
+            self.colleges.append(collegeTextField.text!)
+            self.tableView.reloadData()
+            }
+            alert.addAction(addAction)
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }
 
         @IBOutlet weak var tableView: UITableView!
     
@@ -38,7 +55,13 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             cell.textLabel?.text = colleges[indexPath.row]
             return cell
         }
-    
-
-    }
+        func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+        {
+            if editingStyle == .Delete
+                {
+                    colleges.removeAtIndex(indexPath.row)
+                    tableView.reloadData()
+                }
+        }
+}
 
